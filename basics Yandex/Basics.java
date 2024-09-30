8e4f84567e1408033c4284b538fdba5cb9e21c56
AlexJKL <alexandr_sidorenko@icloud.com>
2024-09-30 08:43:26 +0300

    Git hub

---
+++ basics Yandex/Basics.java
@@ -0,0 +1,6 @@
+public class Basics {
+    public static void main(String[] args) {
+        double rubles = 15000;
+        System.out.print("Сейчас у вас на счёте " + rubles + " рублей.");
+    }
+}
