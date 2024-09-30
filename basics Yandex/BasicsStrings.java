0945b07a36e6f948631979b7e0781868e935466b
AlexJKL <alexandr_sidorenko@icloud.com>
2024-09-30 08:45:24 +0300

    strings

--- basics Yandex/BasicsStrings.java
+++ basics Yandex/BasicsStrings.java
@@ -1,4 +1,4 @@
-public class Basics {
+public class BasicsStrings {
     public static void main(String[] args) {
         double rubles = 15000;
         System.out.print("Сейчас у вас на счёте " + rubles + " рублей.");
